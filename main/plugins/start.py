f7431328e4c4194d5d893d4c88799a8b72a330b0
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-03-24 08:32:21 +0530

    Update start.py
    Updating according to usage

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -15,35 +15,35 @@ async def sett(event):
     msg = await button.get_reply_message() 
     await event.delete()
     async with Drone.conversation(event.chat_id) as conv: 
-        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
+        xx = await conv.send_message("**ʀᴇᴘʟʏ ᴛᴏ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ᴀɴ ɪᴍᴀɢᴇ ғᴏʀ ᴛʜᴜᴍʙɴᴀɪʟ 🖼**")
         x = await conv.get_reply()
         if not x.media:
-            xx.edit("No media found.")
+            xx.edit("**sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ:** ɴᴏ ᴍᴇᴅɪᴀ ғᴏᴜɴᴅ 😒")
         mime = x.file.mime_type
         if not 'png' in mime:
             if not 'jpg' in mime:
                 if not 'jpeg' in mime:
-                    return await xx.edit("No image found.")
+                    return await xx.edit("**sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ:** ɴᴏ ᴍᴇᴅɪᴀ ғᴏᴜɴᴅ 😒")
         await xx.delete()
-        t = await event.client.send_message(event.chat_id, 'Trying.')
+        t = await event.client.send_message(event.chat_id, 'ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...')
         path = await event.client.download_media(x.media)
         if os.path.exists(f'{event.sender_id}.jpg'):
             os.remove(f'{event.sender_id}.jpg')
         os.rename(path, f'./{event.sender_id}.jpg')
-        await t.edit("Temporary thumbnail saved!")
+        await t.edit("ᴛᴇᴍᴘᴏʀᴀʀʏ ᴛʜᴜᴍʙɴᴀɪʟ sᴀᴠᴇᴅ!")
         
 @Drone.on(events.callbackquery.CallbackQuery(data="rem"))
 async def remt(event):  
     Drone = event.client            
-    await event.edit('Trying.')
+    await event.edit('ᴛʀʏɪɴɢ')
     try:
         os.remove(f'{event.sender_id}.jpg')
-        await event.edit('Removed!')
+        await event.edit('ᴍᴇᴅɪᴀ ʀᴇᴍᴏᴠᴇᴅ!')
     except Exception:
-        await event.edit("No thumbnail saved.")                        
+        await event.edit("ɴᴏ sᴀᴠᴇᴅ ᴛʜᴜᴍʙɴᴀɪʟ ғᴏᴜɴᴅ.")                        
   
 @Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
-    text = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @TeamDrone"
+    text = "**ᴘʟᴇᴀsᴇ sᴇɴᴅ ᴀɴʏ ᴍᴇssᴀɢᴇ ᴛᴏ ᴄʟᴏɴᴇ ʜᴇʀᴇ, ғᴏʀ ᴘʀɪᴠᴀᴛᴇ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ sᴇɴᴅ ɪɴᴠɪᴛᴇ ʟɪɴᴋ ᴏғ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ ғɪʀsᴛ**\n\n**SUPPORT:** @WantedBots"
     await start_srb(event, text)
     
